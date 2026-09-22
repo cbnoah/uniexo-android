7889308d1e2cce01543bdc7967f276c17a193e1f
cnoah <noah.charrinbourrat@ynov.com>
2026-09-22 21:46:28 +0200

    feat(googleMap): implement permission handling for location access in GoogleMapScreen

--- app/src/main/java/com/unicofrance/uniexo/ui/googleMap/GoogleMapScreen.kt
+++ app/src/main/java/com/unicofrance/uniexo/ui/googleMap/GoogleMapScreen.kt
@@ -1,17 +1,56 @@
 package com.unicofrance.uniexo.ui.googleMap
 
+import android.Manifest
+import android.content.Context
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
 
 @Composable
 fun GoogleMapScreen(
     modifier: Modifier = Modifier,
     viewModel: GoogleMapViewModel
 ) {
+    val context: Context = LocalContext.current
+
+    val permission by viewModel.permissions.collectAsState()
+
     val location by viewModel.location.collectAsStateWithLifecycle()
 
-    GoogleMap(modifier = modifier)
+    val launcher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { results ->
+        viewModel.onPermissionsResult(results)
+    }
+
+    val mapProperties = MapProperties(
+        isMyLocationEnabled = permission.hasLocationPermissions,
+    )
+
+    LaunchedEffect(Unit) {
+        if (!viewModel.hasPermissions(context).hasLocationPermissions) {
+            launcher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        }
+    }
+
+    GoogleMap(
+        modifier = modifier,
+        properties = mapProperties,
+        contentPadding = PaddingValues(top = 20.dp)
+    )
 }
