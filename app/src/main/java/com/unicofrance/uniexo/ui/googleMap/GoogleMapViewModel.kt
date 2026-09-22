f08db25d4b16f876df5eca68d4cea56b244db467
cnoah <noah.charrinbourrat@ynov.com>
2026-09-22 20:16:20 +0200

    feat(permission): functions for checking already sets permissions and for newly made modifications have been added

--- app/src/main/java/com/unicofrance/uniexo/ui/googleMap/GoogleMapViewModel.kt
+++ app/src/main/java/com/unicofrance/uniexo/ui/googleMap/GoogleMapViewModel.kt
@@ -1,15 +1,53 @@
 package com.unicofrance.uniexo.ui.googleMap
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
 import com.google.android.gms.maps.model.LatLng
 import com.unicofrance.uniexo.data.repositories.ContainerRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class GoogleMapViewModel(
     private val containerRepository: ContainerRepository,
 ) : ViewModel() {
     private val _location = MutableStateFlow<LatLng?>(null)
+
     val location = _location.asStateFlow()
 
+    private val _permissions = MutableStateFlow(PermissionsUiState())
+    val permissions = _permissions.asStateFlow()
+
+    fun hasPermissions(context: Context): PermissionsUiState {
+        val fineLocationGranted = ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+        val coarseLocationGranted = ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+        _permissions.update { currentState ->
+            currentState.copy(
+                hasFineLocation = fineLocationGranted,
+                hasCoarseLocation = coarseLocationGranted
+            )
+        }
+
+        return _permissions.value
+    }
+
+    fun onPermissionsResult(results: Map<String, Boolean>) {
+        _permissions.update { currentState ->
+            currentState.copy(
+                hasFineLocation = results[Manifest.permission.ACCESS_FINE_LOCATION]
+                    ?: currentState.hasFineLocation,
+                hasCoarseLocation = results[Manifest.permission.ACCESS_COARSE_LOCATION]
+                    ?: currentState.hasCoarseLocation
+            )
+        }
+    }
 }
