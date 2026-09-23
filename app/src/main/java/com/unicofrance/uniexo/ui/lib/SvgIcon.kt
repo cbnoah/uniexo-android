61e553dbf5a3736e06e4e2d176a6ec1c0b348985
cnoah <noah.charrinbourrat@ynov.com>
2026-09-23 21:03:54 +0200

    feat(svg): enhance SVG loading with state management and image loader optimization

--- app/src/main/java/com/unicofrance/uniexo/ui/lib/SvgIcon.kt
+++ app/src/main/java/com/unicofrance/uniexo/ui/lib/SvgIcon.kt
@@ -2,9 +2,14 @@ package com.unicofrance.uniexo.ui.lib
 
 import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import coil.ImageLoader
+import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 import coil.decode.SvgDecoder
 
@@ -15,15 +20,26 @@ fun SvgIcon(
 ) {
     val context = LocalContext.current
 
+    val imageLoader = remember(context) {
+        ImageLoader.Builder(context)
+            .components {
+                add(SvgDecoder.Factory())
+            }
+            .build()
+    }
+
+    var isLoader by remember { mutableStateOf(false) }
+
+    val painter = rememberAsyncImagePainter(
+        model = url,
+        imageLoader = imageLoader,
+        onState = { state ->
+            if (state is AsyncImagePainter.State.Success)
+                isLoader = true
+        }
+    )
     Image(
-        painter = rememberAsyncImagePainter(
-            model = url,
-            imageLoader = ImageLoader.Builder(context)
-                .components {
-                    add(SvgDecoder.Factory())
-                }
-                .build()
-        ),
+        painter = painter,
         contentDescription = null,
         modifier = modifier
     )
