052c04ffb44238537fe4633aead56a524844c154
cnoah <noah.charrinbourrat@ynov.com>
2026-09-23 22:04:20 +0200

    feat(navigation): implement navigation between GoogleMapScreen and DetailScreen

--- app/src/main/java/com/unicofrance/uniexo/ui/MainActivity.kt
+++ app/src/main/java/com/unicofrance/uniexo/ui/MainActivity.kt
@@ -6,7 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.unicofrance.uniexo.UniExoApplication
+import com.unicofrance.uniexo.ui.detail.DetailScreen
+import com.unicofrance.uniexo.ui.detail.DetailViewModel
 import com.unicofrance.uniexo.ui.googleMap.GoogleMapScreen
 import com.unicofrance.uniexo.ui.googleMap.GoogleMapViewModel
 
@@ -17,10 +22,34 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            GoogleMapScreen(
-                modifier = Modifier.fillMaxSize(),
-                viewModel = GoogleMapViewModel(containerRepository = app.containerRepository)
-            )
+            val navController = rememberNavController()
+
+            NavHost(navController = navController, startDestination = "map") {
+                composable("map") {
+                    GoogleMapScreen(
+                        modifier = Modifier.fillMaxSize(),
+                        viewModel = GoogleMapViewModel(containerRepository = app.containerRepository),
+                        onNavigationToDetail = { id ->
+                            navController.navigate("detail/$id")
+                        }
+                    )
+                }
+                composable("detail/{id}") { backStackEntry ->
+                    val id = backStackEntry.arguments?.getString("id")
+                    DetailScreen(
+                        modifier = Modifier.fillMaxSize(),
+
+                        backToMap = {
+                            navController.popBackStack()
+                        },
+                        viewModel = DetailViewModel(
+                            containerRepository = app.containerRepository,
+                            containerId = id
+                        )
+                    )
+                }
+            }
+
         }
     }
 }
