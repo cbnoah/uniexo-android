de9230a18eb86edbdfa6888b8775b60217c4011b
cnoah <noah.charrinbourrat@ynov.com>
2026-09-24 13:10:36 +0200

    feat(MainActivity): system bars are now auto hiding (and transparent when shown) for better use of screen space

--- app/src/main/java/com/unicofrance/uniexo/ui/MainActivity.kt
+++ app/src/main/java/com/unicofrance/uniexo/ui/MainActivity.kt
@@ -6,6 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -21,6 +24,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         setContent {
             val navController = rememberNavController()
 
@@ -49,7 +58,6 @@ class MainActivity : ComponentActivity() {
                     )
                 }
             }
-
         }
     }
 }
